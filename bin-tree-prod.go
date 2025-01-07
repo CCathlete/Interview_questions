77df32fe98acc1d5bcd61f780037a1f2014dfff0
CCat <ccathlete01@gmail.com>
2025-01-07 07:56:50 +0100

    Finished (tested) binTree max prod.

--- bin-tree-prod.go
+++ bin-tree-prod.go
@@ -1,22 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"math"
+
+	"github.com/gammazero/deque"
+)
+
 type node struct {
 	level int64
-	val int64
-	left *node
+	val   int64
+	left  *node
 	right *node
 }
 
-var root = *node{level: 0}
+var root = &node{level: 0}
 
 // A helper function for calculating max value.
-func max[T](a, b T) T {
+func Max[T int | int64 | float64](a, b T) T {
 	if a > b {
 		return a
 	}
 
 	return b
 }
+
+var eq string
 
 func treeProduct(root *node) (prod int64) {
 	if root == nil {
@@ -25,29 +34,41 @@ func treeProduct(root *node) (prod int64) {
 	}
 
 	// Using a queue to traverse each level.
-	levelWatchlist := deque.Deque[*node]{root}
+	levelWatchlist := &deque.Deque[*node]{}
+	levelWatchlist.PushFront(root)
 	prod = 1
 
-	for Len(queue) > 0 {
-		max := math.MinInt64 // Closest to -inf without using
-		// float64.
+	for levelWatchlist.Len() > 0 {
+		max := int64(math.MinInt64) // Closest to -inf without using float64.
 
-		parent = levelWatchlist.PopBack()
-		max = max(max, parent.val)
+		parent := levelWatchlist.PopBack()
+		max = Max(max, parent.val)
+		eq += fmt.Sprintf("%d * ", max)
 
 		// If current node has a left child, we add it to our
 		// watchlist.
 		if parent.left != nil {
-			levelWatchlist.PushRight(parent.left)
+			levelWatchlist.PushFront(parent.left)
 		}
 		// If current node has a right child, we add it to our
 		// watchlist.
 		if parent.right != nil {
-			levelWatchlist.PushRight(parent.right)
+			levelWatchlist.PushFront(parent.right)
 		}
 
 		prod *= max
 	}
 
+	eq = fmt.Sprintf("%s =", eq)
 	return
+}
+
+func main() {
+	root.val = 10
+	root.left = &node{level: 1, val: 4}
+	root.right = &node{level: 1, val: 12}
+	root.left.left = &node{level: 2, val: 24}
+	root.left.right = &node{level: 2, val: 12}
+
+	fmt.Println(eq, treeProduct(root))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-inter-q
 
 go 1.22.2
+
+require github.com/gammazero/deque v1.0.0
