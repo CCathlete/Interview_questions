5820d3f89100ff2f60634aad85a25226faeb6a58
ccat <ccathlete01@gmail.com>
2025-01-31 09:07:07 +0100

    Implemented bubble sort in sum2.

--- python/two_sum.py
+++ python/two_sum.py
@@ -15,6 +15,34 @@
 # -------------------------- Helper functions --------------------------
 
 
+def bubble_sort(arr: list[int]) -> list[int]:
+    """
+    Example:
+    i = 0, arr: [3, 2, 1]
+    j=0, [2, 3, 1] >> j=1, [2, 1, 3]
+    i = 1, arr: [2, 1, 3]
+    j=0, [1, 2, 3]
+    i = 2
+    do nothing.
+    """
+    for i in range(len(arr)):
+        # We want range of len -i -1 because we take care of the last element in the interation before the last.
+        for j in range(len(arr) - (i + 1)):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+# -------------------------- Implementation ----------------------------
+
+
+def two_sum(arr: list[int], target: int) -> list[int]:
+    couple: list[int] = []
+
+    return couple
+
+
 # ---------------------------- Test cases ------------------------------
 
 
@@ -24,10 +52,12 @@ class TestCase:
     target: int
 
     def __init__(
-        self, test_name: str,
+        self,
+        test_name: str,
         arr: list[int],
-        target: int
-        ) -> None:
+        target: int,
+    ) -> None:
+
         self.test_name = test_name
         self.arr = arr
         self.target = target
@@ -38,11 +68,28 @@ class TestCase:
 
 def main() -> None:
     test_cases: list[TestCase] = [
-        TestCase{
-            "Happy case: One couple of numbers.",
-            
-        }
+        TestCase(
+            test_name="Happy case: One couple of numbers.",
+            arr=[0, 10, 300, 1, 3, 7],
+            target=1,
+        ),
+        TestCase(
+            test_name="Edge case 1: Empty array.",
+            arr=[],
+            target=1,
+        ),
+        TestCase(
+            test_name="Edge case 2: No matchine couple.",
+            arr=[0, 10, 300, 1, 3, 7],
+            target=50,
+        ),
     ]
+
+    for test_case in test_cases:
+        print(f"Running test case: {test_case.test_name}\n")
+        print(f"Array: {test_case.arr}\n")
+        print(f"Target: {test_case.target}\n")
+        print(f"Result: {two_sum(test_case.arr, test_case.target)}\n")
 
 
 if __name__ == "__main__":
