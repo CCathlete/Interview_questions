02a819afb569d56dfa159901df35263df9878d36
ccat <ccathlete01@gmail.com>
2025-02-02 20:02:15 +0100

    Finished search_rotated_sorted_array.

--- python/search_rotated_sorted_array.py
+++ python/search_rotated_sorted_array.py
@@ -96,10 +96,16 @@ class Test_Case:
 def main() -> None:
     test_cases: list[Test_Case] = [
         Test_Case(
-            name="Happy case",
+            name="Happy case.",
             nums=[4, 5, 6, 7, 8, 1, 2],
             target=1,
             expected_result=5,
+        ),
+        Test_Case(
+            name="Empty array.",
+            nums=[],
+            target=1,
+            expected_result=-1,
         ),
     ]
 
